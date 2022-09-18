2a011b40be2a24467058673d545626e210d6bb7a
Rloundy <rlrjloundy@gmail.com>
2022-09-18 14:04:12 -0500

    Added docstring

--- toolkit/models.py
+++ toolkit/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     Email = models.EmailField( max_length=30, primary_key=True, unique=True)
     Username = models.CharField(max_length=15)
     Password = models.CharField(max_length=20)
