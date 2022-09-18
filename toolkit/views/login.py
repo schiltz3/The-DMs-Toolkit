f21f46eb7c0bfceb80715c38431a93a3f24a12fe
John Tabor <jrtabor@uwm.edu>
2022-09-18 14:16:10 -0500

    Basic login backend structure

---
+++ toolkit/views/login.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.views import View
+# from .models import Account
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, "login.html")
+        
+    def post(self, request):
+        return render(request, "login.html")
+    
+    # Function used to retrieve User from given email address or username and validate password
+    def retrieve_user():
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
