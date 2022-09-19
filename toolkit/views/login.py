68e780ff740d812a8c84a0eff06f31c10ed24843
John Tabor <jrtabor@uwm.edu>
2022-09-19 13:10:13 -0500

    Update to fix import ..models line in login.py

--- toolkit/views/login.py
+++ toolkit/views/login.py
@@ -1,27 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from models import Account
+from ..models import Account
 
 
 class Login(View):
     def get(self, request):
         return render(request, "login.html")
-        
+
     def post(self, request):
         return render(request, "login.html")
-    
+
     # Function used to retrieve User from given email address or username and validate password
-    def retrieve_user():
+    def retrieve_user(self):
         pass
-
-
-
-
-
-
-
-
-
-
-
-
