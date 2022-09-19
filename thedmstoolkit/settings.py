9e7a06e6bbd75f7c5638f383691655e070febb3b
John Schiltz <johnschiltz2000@gmail.com>
2022-09-19 10:11:29 -0500

    Needed to add am eatery for the new directory
    Also added dir for base dir idk if needed

--- thedmstoolkit/settings.py
+++ thedmstoolkit/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "toolkit/static/"
+STATIC_URL = "static/"
+STATICFILES_DIRS = ["toolkit/static/", "toolkit/static/images/"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
