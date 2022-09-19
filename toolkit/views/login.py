93a655c05ea4621190c4d27d5a4f4533a188e371
John Tabor <jrtabor@uwm.edu>
2022-09-19 14:19:37 -0500

    staticmethod decorator to fix docstring failure

--- toolkit/views/login.py
+++ toolkit/views/login.py
@@ -7,11 +7,12 @@ class Login(View):
     A class to handle the login page's GET and POST requests. As well as retrieve
     a user's credentials from the Account database.
     """
-
+    @staticmethod
     def get(self, request):
         """GET method for login page."""
         return render(request, "login.html")
 
+    @staticmethod
     def post(self, request):
         """POST method for login page."""
         return render(request, "login.html")
