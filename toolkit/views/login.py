6225f0a6ceed26339201df71fb411fdbea634ed8
John Tabor <jrtabor@uwm.edu>
2022-09-19 13:51:01 -0500

    Docstrings and other formatting to login.py

--- toolkit/views/login.py
+++ toolkit/views/login.py
@@ -5,12 +5,26 @@ from ..models import Account
 
 
 class Login(View):
+    """
+    A class to handle the login page's GET and POST requests. As well as retrieve 
+    a user's credentials from the Account database.
+    """
+    @staticmethod
     def get(self, request):
+        """
+        GET method for login page.
+        """
         return render(request, "login.html")
 
+    @staticmethod
     def post(self, request):
+        """
+        POST method for login page.
+        """
         return render(request, "login.html")
 
-    # Function used to retrieve User from given email address or username and validate password
     def retrieve_user(self):
-        pass
+        """
+        Function used to retrieve the credentials of a user and validate them or not.
+        """
+        pass # Function to be implemented in separate PR
