71b01ced67aa0530c3f9ad6b4c8c0d1792f36b00
John Tabor <jrtabor@uwm.edu>
2022-09-19 14:02:05 -0500

    Try to fix docstring check failures

--- toolkit/views/login.py
+++ toolkit/views/login.py
@@ -1,7 +1,5 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.views import View
-
-from ..models import Account
 
 
 class Login(View):
@@ -10,22 +8,10 @@ class Login(View):
     a user's credentials from the Account database.
     """
 
-    @staticmethod
     def get(self, request):
-        """
-        GET method for login page.
-        """
+        """GET method for login page."""
         return render(request, "login.html")
 
-    @staticmethod
     def post(self, request):
-        """
-        POST method for login page.
-        """
+        """POST method for login page."""
         return render(request, "login.html")
-
-    def retrieve_user(self):
-        """
-        Function used to retrieve the credentials of a user and validate them or not.
-        """
-        pass  # Function to be implemented in separate PR
