894265c3f3e977c26cb9a897f1c9c52bcf27347f
John Tabor <jrtabor@uwm.edu>
2022-09-18 19:50:20 -0500

    Backend work

--- toolkit/views/login.py
+++ toolkit/views/login.py
@@ -8,11 +8,27 @@ class Login(View):
         return render(request, "login.html")
         
     def post(self, request):
+        formEmail = request.POST['email']
+        formPassword = request.POST['password']
+        valid = retrieve_user(formEmail, formPassword)
+        if valid:
+            return redirect('/home/')
         return render(request, "login.html")
     
     # Function used to retrieve User from given email address or username and validate password
-    def retrieve_user():
-        pass
+    def retrieve_user(self, formEmail, formPassword):
+        try:
+            e = Account.objects.get(email=formEmail)
+            isValid = (e.password == formPassword)
+        except:
+            return False
+        if isValid:
+            return True
+        return False
+        
+
+
+            
 
 
 
