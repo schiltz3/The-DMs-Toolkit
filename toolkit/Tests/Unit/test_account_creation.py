a57d2fccb493667bd5810cad62917bd0d8da9163
John Schiltz <johnschiltz2000@gmail.com>
2022-10-04 00:51:02 -0500

    add task that may not be usefull

--- toolkit/Tests/Unit/test_account_creation.py
+++ toolkit/Tests/Unit/test_account_creation.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from toolkit.models import User
 
 
-class TestLogin(TestCase):
+class TestCreateAccount(TestCase):
     """
     Testing for login page.
     """
@@ -14,36 +14,39 @@ class TestLogin(TestCase):
         each individual test case.
         """
         self.client = Client()
-        self.login_url = reverse("login")
-        self.home_url = "/"
         self.create_account_url = reverse("create_account")
+        self.home_url = "/"
+        self.login_url = reverse("login")
+        self.username = "dummy"
+        self.email = "dummy@email.com"
+        self.password = "dummy_password"
         self.test_user = User.objects.create_user(
-            username="dummy", email="dummy@uwm.edu", password="password"
+            username=self.username, email=self.email, password=self.password
         )
-        self.test_user.save()
+        # self.save()
 
     def test_can_access_login_page(self):
         """
-        Tests to see if a user is able to access the login page.
+        Tests to see if a user is able to access the create account page.
         """
-        response = self.client.get(self.login_url)
+        response = self.client.get(self.create_account_url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "login.html")
+        self.assertTemplateUsed(response, "create_account.html")
 
     def test_successful_login(self):
         """
         Tests if a user is able to login successfully after giving correct credentials
         """
-        user = User.objects.filter(username=self.test_user.username).first()
-        user.is_active = True
-        user.save()
         response = self.client.post(
-            self.login_url,
-            username=self.test_user.username,
-            password=self.test_user.password,
+            self.create_account_url,
+            username=self.username,
+            email=self.email,
+            password=self.password,
         )
         self.assertEqual(response.status_code, 302)
-        # response = self.client.login(username=self.test_user.username, password=self.test_user.password)
+        user = User.objects.filter(username=self.username).first()
+        self.assertEqual(user, self.test_user)
+        # response = self.client.login(username=self.username, password=self.password)
         # self.assertTrue(response)
 
     def test_unsuccessful_login_no_username(self):
@@ -51,55 +54,55 @@ class TestLogin(TestCase):
         Tests if a user is able to login after giving no username
         """
         response = self.client.post(
-            self.login_url,
-            {"username": "", "password": self.test_user.password},
+            self.create_account_url,
+            {"username": "", "password": self.password},
             follow=True,
         )
         self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, self.login_url)
+        self.assertRedirects(response, self.create_account_url)
 
     def test_unsuccessful_login_no_password(self):
         """
         Tests if a user is able to login after giving no password
         """
         response = self.client.post(
-            self.login_url,
-            {"username": self.test_user.username, "password": ""},
+            self.create_account_url,
+            {"username": self.username, "password": ""},
             follow=True,
         )
         self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, self.login_url)
+        self.assertRedirects(response, self.create_account_url)
 
     def test_unsuccessful_login_no_username_password(self):
         """
         Tests if a user is able to login after giving no username or password
         """
         response = self.client.post(
-            self.login_url, {"username": "", "password": ""}, follow=True
+            self.create_account_url, {"username": "", "password": ""}, follow=True
         )
         self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, self.login_url)
+        self.assertRedirects(response, self.create_account_url)
 
     def test_unsuccessful_login_wrong_username(self):
         """
         Tests if a user is able to login after giving incorrect username
         """
         response = self.client.post(
-            self.login_url,
-            {"username": "failure", "password": self.test_user.password},
+            self.create_account_url,
+            {"username": "failure", "password": self.password},
             follow=True,
         )
         self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, self.login_url)
+        self.assertRedirects(response, self.create_account_url)
 
     def test_unsuccessful_login_wrong_password(self):
         """
         Tests if a user is able to login after giving incorrect password
         """
         response = self.client.post(
-            self.login_url,
-            {"username": self.test_user.username, "password": "failure"},
+            self.create_account_url,
+            {"username": self.username, "password": "failure"},
             follow=True,
         )
         self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, self.login_url)
+        self.assertRedirects(response, self.create_account_url)
