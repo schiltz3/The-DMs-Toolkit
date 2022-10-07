d638692a937a4889b59fac7a421130614872d7f3
Rloundy <rlrjloundy@gmail.com>
2022-10-07 17:55:28 -0500

    Added the rest of the Databases and added them to admin panel

--- toolkit/admin.py
+++ toolkit/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Character, User
+from .models import Character, Armor, Weapon, GenericItem, MagicItem, GeneratedLoot, Tag, Monster, GeneratedEncounter
 
 admin.site.register(Character)
+admin.site.register(Armor)
+admin.site.register(Weapon)
+admin.site.register(GenericItem)
+admin.site.register(MagicItem)
+admin.site.register(GeneratedLoot)
+admin.site.register(Tag)
+admin.site.register(Monster)
+admin.site.register(GeneratedEncounter)
