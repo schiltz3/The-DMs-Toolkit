f7fbae3de159dc1f8ba536f21572e3953e8808fb
John Tabor <jrtabor@uwm.edu>
2022-10-17 15:55:00 -0500

    Missed one more staticmethod decorator

--- toolkit/views/logout.py
+++ toolkit/views/logout.py
@@ -6,6 +6,7 @@ from django.views import View
 class Logout(View):
     """View to handle logout requests started from user navbar dropdown selection."""
 
+    @staticmethod
     def custom_logout(request):
         """Logout a user if they are logged in. Redirects back to home page."""
         logout(request)
