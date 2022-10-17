815cc7aa89b803db7d2a46b42d68d59e79c79178
John Tabor <jrtabor@uwm.edu>
2022-10-17 15:44:50 -0500

    Deepsource fixes

--- toolkit/views/saved_encounters.py
+++ toolkit/views/saved_encounters.py
@@ -6,14 +6,14 @@ from django.views import View
 class SavedEncounters(View):
     """A class to handle the retrieval and list of a user's saved encounters."""
     
-    def get(self, request):
+    def get(request):
         """GET method for saved encounters page"""
         if not request.user.is_authenticated:
             messages.warning(request, "You must be logged in to access this page.")
             return redirect("login")
         return render(request, "saved_encounters.html")
     
-    def post(self, request):
+    def post(request):
         """POST method for saved encounters page"""
         if not request.user.is_authenticated:
             messages.warning(request, "You must be logged in to access this page.")
