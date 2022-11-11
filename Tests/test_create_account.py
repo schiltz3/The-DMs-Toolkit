41e9963e8a9081b10ce8f9fb5640d5ff44de3012
Rloundy <rlrjloundy@gmail.com>
2022-11-10 23:35:19 -0600

    create user tests

---
+++ Tests/test_create_account.py
@@ -0,0 +1,18 @@
+import django
+from toolkit.views.create_account import create_user
+from django.test import TestCase
+from toolkit.models import User
+django.setup()
+
+class Account_Creation_Tests(TestCase):
+    def test_create_user_positive(self):
+        create_user("test", "test@test.test","test")
+        check = User.objects.filter(username = "test")
+        self.assertTrue(check.exists)
+        self.assertEqual(check[0].email, "test@test.test")
+        self.assertEqual(check[0].password, "test")
+    def test_create_user_negative(self):
+        temp = User(username = "test2", password = "test2", email = "Test2@Test2.Test2")
+        temp.save()
+        with self.assertRaises(ValueError):
+            create_user("test2","test2@test2.test2", "test2")
