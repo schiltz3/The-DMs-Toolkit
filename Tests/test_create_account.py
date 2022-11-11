baa17e572a66113b3a2686b93e50b2f85666a1d3
John Tabor <jrtabor@uwm.edu>
2022-11-11 16:16:44 -0600

    Update docstrings for tests

--- Tests/test_create_account.py
+++ Tests/test_create_account.py
@@ -5,7 +5,7 @@ from toolkit.views.create_account import CreateAccountForm
 
 
 class TestCreateAccount(TestCase):
-    """Testing for login page."""
+    """Testing for create account and confirm account creation page."""
 
     def setUp(self):
         """
@@ -23,33 +23,33 @@ class TestCreateAccount(TestCase):
         self.email = "dummy@uwm.edu"
 
     def test_can_access_create_account_page(self):
-        """Tests to see if a user is able to access the login page."""
+        """Tests to see if a user is able to access the create user page."""
         response = self.client.get(self.create_account_url, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "create_account.html")
 
     def test_form_valid(self):
-        """Tests if a user is able to login successfully after giving correct credentials."""
+        """Tests if the form is valid after giving correct credentials."""
         form = CreateAccountForm(data={"email": self.email, "username": self.username, "password": self.password})
         self.assertTrue(form.is_valid())
         
     def test_form_invalid_email(self):
-        """Tests if a user is able to login successfully after giving correct credentials."""
+        """Tests if the form is valid after giving incorrect credentials."""
         form = CreateAccountForm(data={"email": "dummy", "username": self.username, "password": self.password})
         self.assertFalse(form.is_valid())
         
     def test_form_invalid_no_email(self):
-        """Tests if a user is able to login successfully after giving correct credentials."""
+        """Tests if the form is valid after giving incorrect credentials."""
         form = CreateAccountForm(data={"email": "", "username": self.username, "password": self.password})
         self.assertFalse(form.is_valid())
         
     def test_form_invalid_no_username(self):
-        """Tests if a user is able to login successfully after giving correct credentials."""
+        """Tests if the form is valid after giving incorrect credentials."""
         form = CreateAccountForm(data={"email": self.email, "username": "", "password": self.password})
         self.assertFalse(form.is_valid())
         
     def test_form_valid_no_password(self):
-        """Tests if a user is able to login successfully after giving correct credentials."""
+        """Tests if the form is valid after giving incorrect credentials."""
         form = CreateAccountForm(data={"email": self.email, "username": self.username, "password": ""})
         self.assertFalse(form.is_valid())
         
