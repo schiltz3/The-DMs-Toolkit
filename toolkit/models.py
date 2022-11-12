90df5a499d12902cfdc7cf065fc72eeb80120844
John Schiltz <johnschiltz2000@gmail.com>
2022-11-12 17:45:24 -0600

    Add cache to user

--- toolkit/models.py
+++ toolkit/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -235,3 +237,19 @@ class GeneratedEncounter(models.Model):
     )
     Encounter_Tags = models.ManyToManyField(Tag, blank=True)
     Monsters = models.ManyToManyField(Monster)
+
+
+class SaveCache(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    character = models.OneToOneField(Character, on_delete=models.CASCADE)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        SaveCache.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
