6beddeabae18b5e3dd9a8f322d9b57295af42383
John Schiltz <johnschiltz2000@gmail.com>
2022-11-12 20:29:19 -0600

    Add caching functionarlity

---
+++ toolkit/views/character_generator/cache_character.py
@@ -0,0 +1,49 @@
+from django.http.request import HttpRequest
+from django.contrib.auth.models import User
+from toolkit.models import Character
+
+from toolkit.views.character_generator.character_elements import (
+    GenerateCharacterInputs,
+    GeneratedCharacterOutputs,
+)
+
+
+def cache_character(
+    request: HttpRequest,
+    input: GenerateCharacterInputs,
+    output: GeneratedCharacterOutputs,
+):
+    user = request.user
+    if type(user) is not User:
+        raise TypeError(
+            f"User is of class {type(user)} when it should be User. Is user not logged in?"
+        )
+
+    character = Character(
+        Owner=user,
+        Name=input.character_name.value,
+        Race=input.race.value,
+        Class=input.clazz.value,
+        Background=input.background.value,
+        Alignment=input.alignment.value,
+        Level=0,
+        Experience=0,
+        Strength=output.strength,
+        Dexterity=output.dexterity,
+        Constitution=output.constitution,
+        Intelligence=output.intelligence,
+        Wisdom=output.wisdom,
+        Charisma=output.charisma,
+    )
+    # one to one relationships get fields passed though at runtime
+    cache = user.cache.character  # type: ignore
+
+    character.save()
+    if cache is not None:
+        cache.delete()
+    cache = character
+    user.save()
+
+
+def retrieve_cached_character(self):
+    pass
