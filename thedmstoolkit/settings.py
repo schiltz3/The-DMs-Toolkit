aac05922f620d0c884aa520af252e0c78864b52f
John Schiltz <johnschiltz2000@gmail.com>
2022-11-12 22:04:01 -0600

    use messages

--- thedmstoolkit/settings.py
+++ thedmstoolkit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,4 +136,12 @@ LOGGING = {
         "handlers": ["console"],
         "level": "INFO",
     },
+}
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "alert-info",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
 }
