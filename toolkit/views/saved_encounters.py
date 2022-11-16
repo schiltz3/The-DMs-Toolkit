b929248e3b07bb3bf544225b64c463c17c86e2f8
Rloundy <rlrjloundy@gmail.com>
2022-11-16 15:34:41 -0600

    Fixed init?

--- toolkit/views/saved_encounters.py
+++ toolkit/views/saved_encounters.py
@@ -9,6 +9,7 @@ class SavedEncounters(View):
     """A class to handle the retrieval and list of a user's saved characters."""
 
     def __init__(self):
+        super(SavedEncounters,self).__init__()
         self.context: dict[str, any] = {}
 
     def get(self, request: HttpRequest):
--- toolkit/views/saved_loot.py
+++ toolkit/views/saved_loot.py
@@ -9,6 +9,7 @@ class SavedLoot(View):
     """A class to handle the retrieval and list of a user's saved characters."""
 
     def __init__(self):
+        super(SavedLoot,self).__init__()
         self.context: dict[str, any] = {}
 
     def get(self, request: HttpRequest):
