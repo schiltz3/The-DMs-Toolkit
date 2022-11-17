a45550daa935aa22dd9f53e3290de423533f9444
John Schiltz <johnschiltz2000@gmail.com>
2022-11-16 23:46:31 -0600

    deepsource

--- toolkit/views/account/change_password.py
+++ toolkit/views/account/change_password.py
@@ -5,13 +5,15 @@ from django.views import View
 
 class ChangePassword(View):
     def __init__(self, **kwargs):
+        """hehe"""
         super().__init__(kwargs)
         self.hehe = "https://r.mtdv.me/blog/posts/changepassword"
 
     def get(self, request: HttpRequest, **kwargs):
+        """hehe"""
         return redirect(self.hehe)
 
     def post(self, request: HttpRequest, **kwargs):
-        """POST method for confirm account creation page."""
+        """hehe"""
         username = kwargs.get("username", "")
         return redirect(self.hehe)
