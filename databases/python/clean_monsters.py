e685f66a568b9eb7f13458d765ed57af23de0547
John Schiltz <johnschiltz2000@gmail.com>
2022-11-25 15:28:49 -0600

    update to use parsers

--- databases/python/clean_monsters.py
+++ databases/python/clean_monsters.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 from json import dump, dumps, load
 from pathlib import Path
 from sys import stdout
-from typing import cast
+from typing import Any, Callable, cast
 
 from loguru import logger
 
@@ -13,31 +13,60 @@ def set_up_parser():
         description="Clean monsters.json generatored by running monsters-1668973282154.json though jq with monsters.jq as the filters",
     )
     parser.add_argument("input")
-    parser.add_argument("-o", "--output")
-    parser.add_argument("-v", "--verbose", action="store_true")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-o", "--output")
+    group.add_argument(
+        "-i",
+        "--inplace",
+        help="Cleans file in place. WARNING: this will overwrite the input file",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-v", "--verbose", help="Info level verbosity", action="store_true"
+    )
+    parser.add_argument("-vv", help="Debug level verbosity", action="store_true")
     return parser
 
 
-tag_path = Path("")
-
-anti_patterns = ["model"]
-patterns = ["Creature_Tags", "Alignment"]
-
-
-def convert(value):
+def convert(value, f: Callable[[Any], str]):
     p_str = value
-    value = str.title(value)
+    try:
+        value = f(value)
+    except ValueError:
+        logger.error(f"Value: `{value}` can not be converted")
     logger.debug(f"{p_str} -> {value}")
     return value
 
 
+default_parser = lambda v: v
+
+
+parsers: dict[str, dict[str, Callable]] = {
+    "Challenge_Rating": {"str": float, "float": default_parser},
+    "Armor_Class": {"str": int},
+    "Hitpoints": {"str": int},
+    "Initiative": {"str": int},
+    "Creature_Tags": {"str": str.title},
+    "Alignment": {"str": str.title},
+}
+
+
 if __name__ == "__main__":
     args = set_up_parser().parse_args()
-    verbose = args.verbose
+    v = args.verbose
+    vv = args.vv
     path = Path(args.input)
-    if not verbose:
+    level = "DEBUG"
+    if not (v or vv):
         logger.disable(__name__)
-    logger.configure(handlers=[{"sink": stdout, "format": "<blue>{message}</blue>"}])
+    elif v:
+        level = "INFO"
+    elif vv:
+        level = "DEBUG"
+
+    logger.configure(
+        handlers=[{"sink": stdout, "format": "<blue>{message}</blue>", "level": level}]
+    )
     monsters: dict = {"error": "error"}
     with open(path) as monsters_f:
         monsters = load(monsters_f)
@@ -45,19 +74,31 @@ if __name__ == "__main__":
             monster = cast(dict, monster)
             fields = monster.get("fields")
             for k, v in fields.items():
-                if k in anti_patterns:
-                    continue
-                if k not in patterns:
+                if k not in parsers:
                     continue
                 match v:
                     case [*values]:
-                        fields[k] = list(map(convert, values))
+                        fields[k] = [
+                            convert(value, parsers[k][type(value).__name__])
+                            for value in values
+                        ]
                     case value:
-                        fields[k] = convert(value)
+                        try:
+                            fields[k] = convert(v, parsers[k][type(v).__name__])
+                        except KeyError as e:
+                            logger.info(f"{k} has no parsing option for {e}")
+                            fields[k] = convert(v, default_parser)
     output_path = args.output
     if output_path:
         output_path = Path(output_path)
-        with open(output_path, "w") as of:
+        try:
+            with open("  " + output_path, "w") as of:
+                dump(monsters, of, indent=4)
+        except (TypeError, OSError) as e:
+            # TODO: make red
+            logger.error("Unable to open output file")
+    elif args.inplace:
+        with open(path, "w") as of:
             dump(monsters, of, indent=4)
     else:
         print(dumps(monsters))
