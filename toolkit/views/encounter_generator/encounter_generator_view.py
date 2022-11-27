dd02c45b9bfa227e80ebb97506af22c115021289
JefferyMeverden <jjmiii@uwm.edu>
2022-11-26 18:39:09 -0600

    Commit 13: Mutiny resolved. On God we bussin.

--- toolkit/views/encounter_generator/encounter_generator_view.py
+++ toolkit/views/encounter_generator/encounter_generator_view.py
@@ -9,7 +9,7 @@ from django.http.request import HttpRequest
 from django.shortcuts import render
 from django.views import View
 
-from toolkit.models import Tag
+from toolkit.models import Tag, Monster
 from toolkit.views.encounter_generator.cache_encounter import (
     cache_encounter,
     delete_cached_encounter,
